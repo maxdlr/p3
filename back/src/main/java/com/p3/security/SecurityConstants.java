9ab5a9e74a974a6e8b8e48287040b310ac63e3a6
maxdlr <contact@maxdlr.com>
2024-11-04 21:26:13 +0100

    remove double jwt expiration time declaration

--- back/src/main/java/com/p3/security/SecurityConstants.java
+++ back/src/main/java/com/p3/security/SecurityConstants.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import javax.crypto.SecretKey;
 
 public class SecurityConstants {
-    @Value("${api.jwtExpirationMs}")
     public static int jwtExpiration = 900000000;
 
     public static final SecretKey JWT_SECRET = Keys.secretKeyFor(SignatureAlgorithm.HS512);
